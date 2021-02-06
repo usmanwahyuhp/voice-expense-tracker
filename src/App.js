b71a610accddb15285aba80eeb7f5b04025949ed
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-02-06 20:45:15 +0700

    Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Grid } from '@material-ui/core';
+
+import Details from './components/Details/Details'
 
 const App = () => {
     return (
         <div>
-            Hello React
+            <Grid container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                    Main
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Details />
+                </Grid>
+            </Grid>
         </div>
     )
 }
