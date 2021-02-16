dc823914e1c99643fcc876942b0ab92103158ae3
usmanwahyuhp <usmanwahyuhp@gmail.com>
2021-02-16 22:12:51 +0700

    format date using utils function

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -5,12 +5,13 @@ import { v4 as uuidv4 } from 'uuid';
 import  useStyle from './styles';
 import { ExpenseTrackerContext } from '../../../context/context';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import formatDate from '../../../utils/formatDate';
 
 const initialState = {
     amount: '',
     category: '',
     type: 'income',
-    date: new Date()
+    date: formatDate(new Date())
 }
 
 const Form = () => {
@@ -55,7 +56,7 @@ const Form = () => {
                 <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
+                <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
